a24477309041195c3c2266772705c9dff4e4e93a
Pipsson <juliusmallya8420@gmail.com>
2025-06-16 00:04:33 +0300

    feat: Refactor layout and page structure; introduce APP_NAME and APP_DESCRIPTION constants

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,16 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import '@/assets/styles/globals.css';
+import { APP_DESCRIPTION, APP_NAME } from '@/lib/constants';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'Prostore',
-  description: 'A modern store built with Next.js',
+  title: {
+    default: APP_NAME,
+    template: `%s | ${APP_NAME}`,
+  },
+  description: APP_DESCRIPTION,
 };
 
 export default function RootLayout({
