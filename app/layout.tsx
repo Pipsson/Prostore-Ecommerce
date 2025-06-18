c5e0665a8610ec79180926fc91bf1b9383c80e81
Pipsson <juliusmallya8420@gmail.com>
2025-06-18 10:41:13 +0300

    feat: add mode toggle component with dropdown menu for theme selection
    - Implemented ModeToggle component to allow users to switch between light, dark, and system themes.
    - Integrated Radix UI dropdown menu for theme selection.
    - Updated package.json to include necessary dependencies for dropdown menu and themes.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,12 @@
-import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
-import '@/assets/styles/globals.css';
-import { APP_DESCRIPTION, APP_NAME } from '@/lib/constants';
 
-const inter = Inter({ subsets: ['latin'] });
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "@/assets/styles/globals.css";
+import { APP_DESCRIPTION, APP_NAME } from "@/lib/constants";
+import { ThemeProvider } from "next-themes";
+
+
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: {
@@ -12,15 +15,24 @@ export const metadata: Metadata = {
   },
   description: APP_DESCRIPTION,
 };
-
+  
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang='en' suppressHydrationWarning>
-      <body className={`${inter.className}`}>{children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <body className={`${inter.className}`}>
+      <ThemeProvider
+      defaultTheme="light"
+      attribute="class"
+      enableSystem
+      disableTransitionOnChange
+      >
+         {children}
+      </ThemeProvider>
+      </body>
     </html>
   );
 }
---
+++ components/shared/header/mode-toggle.tsx
@@ -0,0 +1,58 @@
+"use client";
+
+import React, {  useEffect, useState } from "react";
+import { useTheme } from "next-themes";
+import {
+  DropdownMenu,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuCheckboxItem
+} from "@/components/ui/dropdown-menu";
+import { SunMoon, MoonIcon } from "lucide-react";
+import { Button } from "@/components/ui/button";
+
+function ModeToggle() {
+  const { theme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+  useEffect(()=>{
+    setMounted(true);
+  }, []);   
+  
+  if(!mounted) {
+    return null;
+  }
+
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild className="focus-visible:ring-offset-0 focus-visible:ring-0">
+        <Button variant="ghost">
+          {theme === "system" ? (
+            <SunMoon />
+          ) : theme === "dark" ? (
+            <MoonIcon />
+          ) : (
+            <SunMoon />
+          )}
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuLabel>Appearance</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuCheckboxItem checked = { theme === 'system'} onClick={ () => setTheme("system")}>  
+            System
+        </DropdownMenuCheckboxItem>
+        <DropdownMenuCheckboxItem checked = { theme === 'light'} onClick={ () => setTheme("light")}>  
+            Light
+        </DropdownMenuCheckboxItem>
+         <DropdownMenuCheckboxItem checked = { theme === 'dark'} onClick={ () => setTheme("dark")}>  
+            Dark
+        </DropdownMenuCheckboxItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
+
+export default ModeToggle;
