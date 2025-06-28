828bf2361ea2a5a455c0755cb4df5c0cbded6589
Pipsson <juliusmallya8420@gmail.com>
2025-06-28 11:32:43 +0300

    feat: enhance sign-in form with default values and link to sign-up page

--- app/(auth)/sign-in/credentials-signin-form.tsx
+++ app/(auth)/sign-in/credentials-signin-form.tsx
@@ -2,6 +2,8 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { DEFAULT_SIGN_IN_VALUES } from "@/lib/constants";
+import Link from "next/link";
 import React from "react";
 
 const CredentialsSignInForm = () => {
@@ -16,6 +18,8 @@ const CredentialsSignInForm = () => {
             name="email"
             required
             placeholder="Enter your email"
+            autoComplete="email"
+            defaultValue={DEFAULT_SIGN_IN_VALUES.email}
           />
         </div>
         <div>
@@ -25,9 +29,14 @@ const CredentialsSignInForm = () => {
             id="password"
             name="password"
             placeholder="Enter your password"
+            defaultValue={DEFAULT_SIGN_IN_VALUES.password}
+            autoComplete="current-password"  
           />
         </div>
-        <Button type="submit">Sign In</Button>
+        <Button type="submit" className="w-full " variant="default">Sign In</Button>
+        <div className="text-sm text-center text-muted-foreground">
+            Don&apos;t have an account? {''} <Link href="/sign-up" target="_self" className="link">Sign Up</Link>
+        </div>
       </div>
     </form>
   );
