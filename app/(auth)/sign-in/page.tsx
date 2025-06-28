fb7be4985c0ed8d8ef27f498e631ca63d84bf64e
Pipsson <juliusmallya8420@gmail.com>
2025-06-28 10:32:16 +0300

    feat: implement user authentication flow with sign-in and sign-up endpoints

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,37 @@
+import { Card, CardDescription, CardHeader, CardTitle, CardContent } from '@/components/ui/card'
+import { Metadata } from 'next'
+import Link from 'next/link'
+import Image from 'next/image'
+import { APP_NAME } from '@/lib/constants'
+
+export const metadata : Metadata = {
+    title : 'Sign In',
+    description : 'Sign In to your account',
+}
+const SignInPage = () => {
+  return (
+    <div 
+    className='w-full max-w-md mx-auto' 
+    >
+        <Card>
+            <CardHeader className='space-y-4'>
+                <Link href='/' className='flex-center'>
+                    <Image  src='/images/logo.svg' alt={`${APP_NAME} logo`} width={100} height={100}  priority = {true} />
+                </Link>
+                <CardTitle className='text-center '>
+                    Sign In
+                </CardTitle>
+                <CardDescription className='text-center'>
+                    Sign in to your account to continue
+                </CardDescription>
+                <CardContent>
+
+
+                </CardContent>
+            </CardHeader>
+        </Card>
+    </div>
+  )
+}
+
+export default SignInPage
